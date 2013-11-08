405f1410cb1a63aa682e310fd1609ba54497c612
Chandu <chandra.vedantham@gmail.com>
2013-11-07 23:43:47 -0500

    Added a coming soon page

---
+++ src/Gittu.Web/Core/GittuNancyBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using Nancy.Conventions;
+
+namespace Gittu.Web.Core
+{
+	public class GittuNancyBootstrapper:DefaultNancyBootstrapper
+	{
+		protected override void ConfigureConventions(NancyConventions nancyConventions)
+		{
+			base.ConfigureConventions(nancyConventions);
+			nancyConventions.StaticContentsConventions.Add(
+				StaticContentConventionBuilder.AddDirectory("public", @"public")
+				);
+			nancyConventions.StaticContentsConventions.Add(
+				StaticContentConventionBuilder.AddDirectory("app", @"public/app")
+				);
+		}
+	}
+}
---
+++ src/Gittu.Web/Modules/DefaultModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Gittu.Web.Modules
+{
+	public class DefaultModule:NancyModule
+	{
+		public DefaultModule()
+		{
+			Get["/"] = _ => View["Home.html", new {Name="Chandu"}];
+		}
+	}
+}
